1e0b4d27f68900976c3f272617c09622e7e0dfaa
mdesanty1 <mndesanty@wpi.edu>
2023-09-13 19:50:50 -0400

    Adding necessary functions to AI file

--- project1/groupname.py
+++ project1/groupname.py
@@ -1,6 +1,18 @@
 #This is a test
 #from game import Game
+import numpy as np
+import pygame
 
-#class GroupName:
-#    def __init__():
-#    def run(self):
+class GroupName:
+    def __init__(self):
+        self.
+    def run(self):
+
+    #Takes in current board and last move
+    def decideMove(lastMove):
+        
+        updateBoard(move)
+
+    #Update board based on AI's last move
+    def updateBoard():
+    
